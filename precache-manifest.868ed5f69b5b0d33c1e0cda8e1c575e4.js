1abf33f69ce87811beae7c71975cd171798daa8e
Peter McDonald <pbmcdonald7@gmail.com>
2019-07-12 16:35:45 -0500

    Updates

---
+++ precache-manifest.868ed5f69b5b0d33c1e0cda8e1c575e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaa87c615dbe0990bc93f32d305004f",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "942cd35e9e8553f42815",
+    "url": "/project-4-assessment/static/css/main.acce167a.chunk.css"
+  },
+  {
+    "revision": "5b8bbdbb75b8b44c442b",
+    "url": "/project-4-assessment/static/js/2.6fcf23cc.chunk.js"
+  },
+  {
+    "revision": "942cd35e9e8553f42815",
+    "url": "/project-4-assessment/static/js/main.2379d11d.chunk.js"
+  },
+  {
+    "revision": "aa123970de1f72af151a",
+    "url": "/project-4-assessment/static/js/runtime~main.4998de7c.js"
+  }
+]);
